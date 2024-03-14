5e2c30bd8fb7bb105da375c92fc3844db821bced
이우성 <sitpo96@naver.com>
2024-03-14 17:47:44 +0900

    Refact #9: 버튼 컨테이너 컴포넌트 분리

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,10 +1,10 @@
-import { Link, useLocation, useParams } from 'react-router-dom'
-import GoogleLoginBtn from './components/GoogleLoginBtn'
-import KakaoLoginBtn from './components/KakaoLoginBtn'
 import { useEffect } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
+
 import { doLogin } from './service'
 import { isLoginTokenType } from 'src/utility/utils/typefilter'
 import ServiceLogo from './components/ServiceLogo'
+import LoginButtonContainer from './components/LoginButtonContainer'
 
 const Login = () => {
 	const { loginType } = useParams()
@@ -19,13 +19,7 @@ const Login = () => {
 	return (
 		<section className="flex flex-col justify-center h-screen px-5">
 			<ServiceLogo />
-			<div className="flex flex-col items-center gap-3">
-				<KakaoLoginBtn />
-				<GoogleLoginBtn />
-				<Link to={'/'} className="border-b-2 text-lightGray1 border-lightGray1">
-					로그인 없이 둘러보기
-				</Link>
-			</div>
+			<LoginButtonContainer />
 		</section>
 	)
 }
