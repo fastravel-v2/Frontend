21f6feb67edbb834934a137e5eaf1fb680ecc7c0
Junsu <s2s2s2x101@gmail.com>
2024-03-24 17:46:16 +0900

    Feat #16: URL리스트 로티보완, 컴포넌트쪼개기

--- frontend/src/pages/UrlBook/components/UrlList.tsx
+++ frontend/src/pages/UrlBook/components/UrlList.tsx
@@ -1,18 +1,34 @@
 // src/UrlBook/components/UrlList.tsx
 
-import UrlItem from './UrlItem';
-import { useUrlStore } from '../store';
+import UrlItem from './UrlItem'
+import { useUrlStore } from '../store'
+import TravelBus2 from '../../../assets/lottie/TravelBus2.json'
+import Lottie from 'react-lottie'
 
 const UrlList: React.FC = () => {
-  const urls = useUrlStore((state) => state.urls);
+	const urls = useUrlStore((state) => state.urls)
 
-  return (
-    <div>
-      {urls.map((url, index) => (
-        <UrlItem key={index} index={index} {...url} />
-      ))}
-    </div>
-  );
-};
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: TravelBus2, // Lottie 애니메이션 데이터
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice',
+		},
+	}
 
-export default UrlList;
+	return (
+		<div>
+			{urls.length > 0 ? (
+				urls.map((url, index) => <UrlItem key={index} index={index} {...url} />)
+			) : (
+				<div className="text-center py-12">
+					<Lottie options={defaultOptions} height={300} width={300} />
+					<h1 className="text-xl font-bold">삐뽀삐뽀 URL 추가 요망</h1>
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default UrlList
