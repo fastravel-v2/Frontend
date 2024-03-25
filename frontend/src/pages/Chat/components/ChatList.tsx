1c90bc4dd3e39076107548dcbb9d38b1121fffb0
이준수 <s2s2s2x101@gmail.com>
2024-03-25 17:58:06 +0900

    Feat #14: 채팅 이미지

--- frontend/src/pages/Chat/components/ChatList.tsx
+++ frontend/src/pages/Chat/components/ChatList.tsx
@@ -6,16 +6,22 @@ const ChatList: React.FC = () => {
 	const messages = useChatStore((state) => state.messages)
 	const chatListRef = useRef<HTMLDivElement>(null) // 채팅 리스트를 위한 ref
 
-	// 메시지 목록이 변경될 때마다 실행
+	//수정: 메시지 목록이 변경될 때마다 실행
+	// 사진 보내면 로딩되기 전에 이동하려고 해서 상단만 보임
+	// 텀 두면 되긴 하는데 자연스럽지 않구만..
 	useEffect(() => {
 		if (chatListRef.current) {
-			chatListRef.current.scrollTop = chatListRef.current.scrollHeight
+			chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+			setTimeout(() => {
+				if (chatListRef.current) {
+					chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+				}
+			}, 0.0001);
 		}
-	}, [messages]) // messages 상태가 변경될 때마다 useEffect가 트리거됩니다.
-
+	}, [messages]);
 	return (
 		// 채팅 리스트의 ref를 여기에 연결합니다.
-		<div ref={chatListRef} className="p-3 overflow-y-auto" style={{ height: '500px' }}>
+		<div ref={chatListRef} className="px-3 overflow-y-auto" style={{ height: '520px' }}>
 			{messages.map((message, index) => (
 				<ChatMessage
 					key={message.id}
