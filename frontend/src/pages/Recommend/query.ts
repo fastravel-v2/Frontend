bf2883f297f2907b8b6cc9eee491cd1ae1d7278d
이우성 <sitpo96@naver.com>
2024-03-25 17:51:45 +0900

    Feat #22: useQuries 적용

--- frontend/src/pages/Recommend/query.ts
+++ frontend/src/pages/Recommend/query.ts
@@ -1,26 +1,26 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQueries, useQueryClient } from '@tanstack/react-query'
 import { getRecommendList } from './api'
 import { useEffect, useMemo, useState } from 'react'
 
-// const categoryInfo = {
-// 	'자연 및 야외 활동': 1,
-// 	'스포츠 및 레저': 2,
-// 	'문화 및 역사': 3,
-// 	'쇼핑 및 시장': 4,
-// 	'음식 및 식당': 5,
-// 	'숙박 및 휴식': 6,
-// 	'체험 및 교육': 7,
-// 	'공연 및 엔터테인먼트': 8,
-// 	'건축물 및 구조물': 9,
-// 	'여행 및 관광 서비스': 10,
-// 	기타: 11,
-// }
+const categoryInfo = {
+	'자연 및 야외 활동': 1,
+	'스포츠 및 레저': 2,
+	'문화 및 역사': 3,
+	'쇼핑 및 시장': 4,
+	'음식 및 식당': 5,
+	'숙박 및 휴식': 6,
+	'체험 및 교육': 7,
+	'공연 및 엔터테인먼트': 8,
+	'건축물 및 구조물': 9,
+	'여행 및 관광 서비스': 10,
+	기타: 11,
+}
 
 // 모든 장소 데이터에 이미지가 있는지 확인해주는 함수
 const checkEveryLocationHasImage = (recommendData: RecommendItemResInfo[]) => {
-	return (
-		recommendData.filter((recommendLocation) => recommendLocation.image_url)
-			.length === recommendData.length
+	return recommendData.every(
+		(recommendLocation) =>
+			recommendLocation.image_url && recommendLocation.image_url?.length > 0
 	)
 }
 
@@ -71,13 +71,60 @@ export const useRecommendListHasImage = (
 	return recommendListWithImage
 }
 
-export const useRecommendCategoryList = (categoryId: number) => {
-	const { data, isLoading } = useQuery<RecommendItemResInfo[]>({
-		queryKey: ['recommendList', categoryId],
-		queryFn: getRecommendList,
+export const useRecommendCategoryList = () => {
+	const queryClient = useQueryClient() // React Query Client 인스턴스를 가져옴
+
+	const everyRecommendListInfo = useMemo(() => {
+		const result: Record<number, RecommendItemResInfo[]> = {}
+		Object.values(categoryInfo).forEach((categoryId) => {
+			result[categoryId] = []
+		})
+
+		return result
+	}, [])
+
+	const recommendListQueriesResults = useQueries({
+		queries: Object.values(categoryInfo).map((categoryId) => {
+			return {
+				queryKey: ['recommendList', categoryId],
+				queryFn: getRecommendList,
+			}
+		}),
 	})
 
-	const recommendListWithImage = useRecommendListHasImage(categoryId, data)
+	useEffect(() => {
+		recommendListQueriesResults.forEach((result, index) => {
+			const categoryId = Object.values(categoryInfo)[index]
+			if (result.isSuccess && result.data.some((item) => !item.image_url)) {
+				queryClient.refetchQueries({ queryKey: ['recommendList', categoryId] })
+			} else if (result.isSuccess) {
+				everyRecommendListInfo[categoryId] = result.data
+			}
+		})
+	}, [recommendListQueriesResults, categoryInfo, queryClient])
 
-	return { recommendListWithImage, isLoading }
+	// :: 이미지가 있는 데이터 반환
+	// - 이미지가 없는 데이터거나 데이터가 비었다면 빈 배열 반환
+	const recommendListWithImage = useMemo(() => {
+		if (
+			Object.values(everyRecommendListInfo).some(
+				(recommendList) => recommendList.length === 0
+			)
+		) {
+			return []
+		}
+
+		const result = Object.values(everyRecommendListInfo).map((recommendList) =>
+			recommendList.map((locationInfo) => ({
+				id: locationInfo.spot_id,
+				name: locationInfo.name,
+				address: locationInfo.address,
+				image: locationInfo.image_url,
+			}))
+		)
+
+		return result
+	}, [everyRecommendListInfo])
+
+	return { recommendListWithImage }
 }
