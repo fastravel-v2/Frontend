9e7675f9663b6f88e4a1a9fbd1f506fff2bb782e
김형규 <dhe0814@naver.com>
2024-03-28 17:33:07 +0900

    Fix #13: api 링크 수정

--- frontend/src/pages/LocationDetail/api.ts
+++ frontend/src/pages/LocationDetail/api.ts
@@ -7,7 +7,7 @@ export const getLocationDetail = async (id: string):Promise<LocationDetailType> 
     // const locationDetail = await instance.get(`location/${id}`)
     // return locationDetail.data
     try {
-        const response = await axios.get(`http://192.168.100.154:8000/spot/${id}`, {
+        const response = await axios.get(`https://j10d204.p.ssafy.io/api/core/place/${id}`, {
             headers: {
                 'INTERNAL_ID_HEADER': '8b5b03b7-ae9f-458e-a2b9-558eac541629'
             }
