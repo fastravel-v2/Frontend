8a266736b83ac0b49c2f03a4ba85d6a94a63bd48
이우성 <sitpo96@naver.com>
2024-03-29 09:06:28 +0900

    Feat: 배포 테스트를 위한 로그인, 토큰 검증 api 요청 메서드 수정

--- frontend/src/pages/Login/api.ts
+++ frontend/src/pages/Login/api.ts
@@ -1,9 +1,16 @@
-import { instance } from 'src/utility/apis/axios'
+import axios from 'axios'
+// import { instance } from 'src/utility/apis/axios'
 
 export const getGoogleLogin = async (code: string) => {
-	await instance.get(`auth/google?code=${code}`)
+	// await instance.get(`auth/google?code=${code}`)
+	await axios.get(
+		`${import.meta.env.VITE_OAUTH_BASE_URL}auth/google?code=${code}`
+	)
 }
 
 export const getKakaoLogin = async (code: string) => {
-	await instance.get(`auth/kakao?code=${code}`)
+	// await instance.get(`auth/kakao?code=${code}`)
+	await axios.get(
+		`${import.meta.env.VITE_OAUTH_BASE_URL}auth/google?code=${code}`
+	)
 }
