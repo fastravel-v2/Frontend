a14d6698516062ce9b62a98c3d40cc27a7b6efd1
leewooseong <sitpo96@naver.com>
2024-04-01 06:53:57 +0900

    Feat #71: 검색 페이지 레이아웃 추가

--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -1,5 +1,19 @@
+import SearchHeader from 'src/components/header/SearchHeader'
+import DefaultLayout from 'src/components/layout/DefaultLayout'
+
 const Search = () => {
-	return <div>Search</div>
+	const handleSearch = async (searchText: string) => {
+		console.log(searchText)
+	}
+
+	return (
+		<DefaultLayout>
+			<SearchHeader
+				placeHolder="여행지를 검색해주세요."
+				handleSearch={handleSearch}
+			/>
+		</DefaultLayout>
+	)
 }
 
 export default Search
