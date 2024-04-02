7ef5b0f1f1599d7ad13c2b45ee6312f613786db2
김형규 <dhe0814@naver.com>
2024-04-02 15:53:19 +0900

    Feat #25: 헤더 뒤로 버튼 기능 추가

--- frontend/src/pages/TravelDetail/index.tsx
+++ frontend/src/pages/TravelDetail/index.tsx
@@ -7,10 +7,12 @@ import { FaArrowLeft } from "react-icons/fa6"
 import Header from "./components/Header"
 import TravelPlanHeader from "./components/TravelPlanHeader"
 import DefaultHeader from "src/components/header/DefaultHeader"
+import { useRouter } from "src/hooks/useRouter"
 
 const TravelDetail = () => {
   const [travelData, setTravelData] = useState<ITravelDetail | undefined>(undefined)
   const {id} = useParams()
+  const router = useRouter()
 
   const refetch =async () => {
     if (id) {
@@ -30,12 +32,13 @@ const TravelDetail = () => {
   }
 
   const headerFunc: IMenuFunc = {
-    left_func: undefined,
+    left_func: router.goBack(),
     right_func: undefined
   }
 
   if (!travelData) {
-    return null
+    router.routeTo('/notFound')
+    return
   }
 
   return (
