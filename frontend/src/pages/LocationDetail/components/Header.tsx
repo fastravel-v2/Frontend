1034c93eefe1dc3161e9277a0f27ea16462758e1
이우성 <sitpo96@naver.com>
2024-04-04 07:30:01 +0900

    Fix: 여행지 상세 페이지 메모 버튼 수정

--- frontend/src/pages/LocationDetail/components/Header.tsx
+++ frontend/src/pages/LocationDetail/components/Header.tsx
@@ -1,31 +1,43 @@
 import MemoModal from 'src/components/MemoModal'
 import { createPortal } from 'react-dom'
-import { useState } from "react"
-import { PiNote } from "react-icons/pi";
+import { useState } from 'react'
+import { PiNote } from 'react-icons/pi'
 
 interface HeaderProps {
-  locationId: string;
-  name: string;
-  address: string;
-  memo: string;
+	locationId: string
+	name: string
+	address: string
+	memo: string
 }
 
-const Header = ({name, address, locationId, memo}: HeaderProps) => {
-  const [isOpen, setIsOpen] = useState(false)
+const Header = ({ name, address, locationId, memo }: HeaderProps) => {
+	const [isOpen, setIsOpen] = useState(false)
 
-  const handleOpenMemoModal = () => {
+	const handleOpenMemoModal = () => {
 		setIsOpen(true)
 	}
 	const handleCloseMemoModal = () => {
 		setIsOpen(false)
 	}
 
-  return (
-    <div className="mt-6 ml-1 mb-4">
-      <h1 className="text-2xl font-bold">{name}</h1>
-      <div className="mb-1 text-darkGray3 flex items-center" onClick={handleOpenMemoModal}><PiNote />메모하기</div>
-      <div className="text-xs font-semibold">{address}</div>
-      {isOpen &&
+	return (
+		<div className="mt-6 ml-1 mb-4">
+			<h1 className="text-2xl font-bold">{name}</h1>
+			<div
+				className="mb-1 text-darkGray3 flex items-center"
+				onClick={handleOpenMemoModal}
+			>
+				{memo ? (
+					<>
+						<PiNote />
+						메모하기
+					</>
+				) : (
+					<>{memo}</>
+				)}
+			</div>
+			<div className="text-xs font-semibold">{address}</div>
+			{isOpen &&
 				createPortal(
 					<MemoModal
 						requestId={locationId}
@@ -34,8 +46,8 @@ const Header = ({name, address, locationId, memo}: HeaderProps) => {
 					/>,
 					document.body
 				)}
-    </div>
-  )
+		</div>
+	)
 }
 
 export default Header
