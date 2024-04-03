e4b2157d259e6a973a54d2fcc16e7d23c87e9d4f
이우성 <sitpo96@naver.com>
2024-04-04 04:30:01 +0900

    Fix: 로그아웃 시 로그인 페이지 이동하도록 수정

--- frontend/src/components/LogoutButton.tsx
+++ frontend/src/components/LogoutButton.tsx
@@ -1,12 +1,16 @@
+import { useRouter } from 'src/hooks/useRouter'
 import { getUserLogout } from 'src/utility/apis/user'
 
 // 유효한 토큰인지를 전역 상태로 관리하고 해당 state를 통해 라우터 프로텍션을 수행한다.
 // 로그아웃 버튼을 누르면 deleteUserLogin 함수를 호출하여 로그아웃을 수행한다.
 const LogoutButton = () => {
+	const { routeTo } = useRouter()
+
 	const handleLogout = async () => {
 		const logoutRes = await getUserLogout()
+
 		if (logoutRes === 'success') {
-			window.location.reload()
+			routeTo('/login')
 		}
 	}
 	return (
